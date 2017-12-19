f6accf747428953ef9733f50d4593a8b9d9bec0a
zhaoming <275169263@qq.com>
2017-12-19 08:37:36 +0800

    8

--- app1/ReleaseApp.sh
+++ app1/ReleaseApp.sh
@@ -2,27 +2,23 @@
 # 获取短版本号
 GITHASH=`git rev-parse --short HEAD`
 APPNAME='app1'
-PUBLISHFOLDER='ReleaseApp'
 APPPORT='5007'
+PUBLISHFOLDER='ReleaseApp'
+DOCKERREGISTRY='10.1.4.222:9999'  
 CURTime="`date +%Y-%m-%d-%H-%m`"
 echo
 echo ---------------版本号为...------------------
 echo $GITHASH
 echo ---------------服务名称...------------------
-echo
 echo $APPNAME
-echo
 echo ---------------发布...------------------
 echo
-dotnet publish $APPNAME  -c Release -o $PUBLISHFOLDER
+echo    `dotnet publish $APPNAME  -c Release -o $PUBLISHFOLDER`
 echo
-echo
-echo ---------------跳到制定目录------------------
+echo ---------------跳到指定目录------------------
 echo
 cd  $APPNAME/
 echo
-
-
 echo ---------------Build镜像...------------------
 echo
 docker build -t $APPNAME:$GITHASH .
@@ -30,10 +26,10 @@ echo
 echo ---------------镜像打标签...------------------
 echo
 #docker tag $APPNAME:$GITHASH $APPNAME:$CURTime
-docker tag $APPNAME:$GITHASH 10.1.4.222:9999/$APPNAME:$CURTime  
+docker tag $APPNAME:$GITHASH $DOCKERREGISTRY/$APPNAME:$CURTime  
 echo ---------------推送镜像...------------------
 echo
-docker push 10.1.4.222:9999/$APPNAME:$CURTime  
+docker push $DOCKERREGISTRY/$APPNAME:$CURTime  
 echo
 echo
 echo ---------------移除容器...------------------
@@ -42,4 +38,4 @@ docker -H tcp://10.1.4.223:2375 rm -f $APPNAME || true
 echo
 echo ---------------启动容器...------------------
 echo
-docker -H tcp://10.1.4.223:2375 run --name $APPNAME -d -p $APPPORT:5000 --env ASPNETCORE_ENVIRONMENT=Development 10.1.4.222:9999/$APPNAME:$CURTime
+docker -H tcp://10.1.4.223:2375 run --name $APPNAME -d -p $APPPORT:5000 --env ASPNETCORE_ENVIRONMENT=Development $DOCKERREGISTRY/$APPNAME:$CURTime
