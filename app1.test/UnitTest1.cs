1e94b28af6f51ddf5eb70c69690d92ea23317596
zhaoming <275169263@qq.com>
2017-12-20 09:22:38 +0800

    9

---
+++ app1.test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace app1.test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.Equal(1,1);
+        }
+    }
+}
