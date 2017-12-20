a1559d7af34528b49d6771031af5d6d843d9cc20
zhaoming <275169263@qq.com>
2017-12-20 16:36:46 +0800

    78

--- app1/Build.sh
+++ app1/Build.sh
@@ -1,10 +1,10 @@
-﻿#!/bin/bash
+#!/bin/bash
 APPNAME='app1'
 APPPORT='5007'
 DOCKERREGISTRY='10.1.4.222:9999'  
 #APPHOST=('10.1.4.223:2375' '10.1.4.222:2375') 
 APPHOST=('10.1.4.222:2375') 
-# 获取GIT短版本号
+# ��ȡGIT�̰汾��
 GITHASH=`git rev-parse --short HEAD`
 CURTIME="`date +%Y-%m-%d-%H-%m`"
 PUBLISHFOLDER='ReleaseApp'
@@ -12,51 +12,51 @@ appPath="$WORKSPACE/$JOB_NAME/"
 # testPath="$WORKSPACE/$JOB_NAME.test/"  
 # testLog="$WORKSPACE/$JOB_NAME.test/test-$GITHASH.log"  
 # if [ ! -d "$testPath" ];then  
-# echo "没找到单元测试项目"
+# echo "û�ҵ���Ԫ������Ŀ"
 # exit 1
 # fi
-#echo---------------检测测试结果...------------------ 
+#echo---------------�����Խ��...------------------ 
 #if [ ! -f "$testLog" ];then
-#echo "没找到单元测试报告文件"
+#echo "û�ҵ���Ԫ���Ա����ļ�"
 #exit 1
 #fi
 #lastLineLog=`tail -1 $testLog`
 #SUFFIX="code of 1"
 #if [[ lastLineLog == *$SUFFIX ]];then
-#echo "单元测试未通过"
+#echo "��Ԫ����δͨ��"
 #exit 1
 #fi
-echo ---------------跳到服务目录 ------------------
+echo ---------------��������Ŀ¼ ------------------
 cd  $appPath
 echo 
-echo ---------------开始发布.......------------------ 
+echo ---------------��ʼ����.......------------------ 
 echo $(`pwd`)
 echo  
    #dotnet publish $APPNAME  -c Release -o $PUBLISHFOLDER
    dotnet publish -c Release -o $PUBLISHFOLDER
 echo 
-echo ---------------Build镜像...------------------
+echo ---------------Build����...------------------
 echo
  docker build -t $APPNAME:$GITHASH .
 echo
-echo ---------------镜像打标签...------------------
+echo ---------------������ǩ...------------------
 echo
     #docker tag $APPNAME:$GITHASH $APPNAME:$CURTIME
     docker tag $APPNAME:$GITHASH $DOCKERREGISTRY/$APPNAME:$CURTIME  
-echo ---------------推送镜像...------------------
+echo ---------------���;���...------------------
 echo
      docker push $DOCKERREGISTRY/$APPNAME:$CURTIME  
 echo
 	for HOST in ${APPHOST[@]}  
 	do  
-echo ---------------操作主机${HOST}--------------- 
+echo ---------------��������${HOST}--------------- 
 echo
 echo
-echo ---------------移除容器...------------------
+echo ---------------�Ƴ�����...------------------
 echo
      docker -H tcp://${HOST} rm -f $APPNAME || true
 echo
-echo ---------------启动容器...------------------
+echo ---------------��������...------------------
 echo
      docker -H tcp://${HOST} run --name $APPNAME -d -p $APPPORT:5000 --env ASPNETCORE_ENVIRONMENT=Development $DOCKERREGISTRY/$APPNAME:$CURTIME
 echo
